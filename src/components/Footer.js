a9ef74ec874a97e518a8026506bc52217069bf9e
CloByrne <116455522+CloByrne@users.noreply.github.com>
2023-04-21 20:17:47 +0100

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Project Fitness</p>
+    </footer>
+  );
+}
+
+export default Footer;
