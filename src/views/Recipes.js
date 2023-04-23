a111f2a73adacd59c42c7dc638e6360714c55e8e
CloByrne <116455522+CloByrne@users.noreply.github.com>
2023-04-23 10:31:00 +0100

    Updates for testing

--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -9,7 +9,7 @@ function Recipes() {
       <div>
         <div className="health-container">
           <div className="health-text">
-            <h1>Welcome to the Health page</h1>
+            <h1>Welcome to the Recipes page</h1>
             <div className="health-sub-text">
               <p>We offer a variety of resources and information to help you achieve your health and fitness goals</p>
             </div>
