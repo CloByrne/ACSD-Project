1deac005de8c0050bf42f1b8fa0c232fefecd3a9
DenisWAD <denismurray2@gmail.com>
2023-04-27 03:06:50 +0100

    Created About view, and associated components. Added different images as background pics to Fitness and About views. Added media queries to About

--- src/views/About.js
+++ src/views/About.js
@@ -1,16 +1,40 @@
 import '../styles/Navbar.css';
 import '../styles/About.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AboutUsBlurb from '../components/AboutUsBlurb';
 
 function About() {
     return (
-        <div className="about-container">
-            <div className="about-text">
-                <h1>Welcome to the About page</h1>
-                <div className="about-sub-text">
-                    <p>We offer a variety of resources and information to help you achieve your health and fitness goals</p>
-                    <p>TEST CHANGE FOR COMMIT</p>
+        <div id="about-view">
+            <div id="showcase" className="about-container">
+                <div className="about-text">
+                    <h1>About Us</h1>
+                    <div className="about-sub-text">
+                        <h3>Learn about why we started out journey towards delivering best in class fitness options for people of all kinds</h3>
+                    </div>
                 </div>
             </div>
+
+            <Container fluid >
+                <Row  id="about-us-section" >
+                    <Col className="blurb" xs={12} lg={7}>
+                        <AboutUsBlurb />
+                    </Col>
+                    <Col className="blurb-pic" xs={12} lg={5}>
+                        <div></div>
+                    </Col>
+
+
+                </Row>
+
+
+
+
+            </Container>
+            <div className="endDiv"></div>
+
         </div>
     )
 }
